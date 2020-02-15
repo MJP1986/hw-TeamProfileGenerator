59ea592e4a0717aad48e58baa2f9c22f38c29982
MinJin Park <parkmin1986@gmail.com>
2020-02-14 19:00:50 -0500

    basic layout

---
+++ app.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
